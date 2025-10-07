5db7aeac4da5b9a39d85a0078149cf8e527abfbe
Spit365 <lukas.grossauer47@gmail.com>
2025-10-07 14:14:41 +0200

    Continue adding the gazebo (Why does no one tell you how to generate structures with java?)

--- src/main/java/net/spit365/lulasmod/mod/ModStructures.java
+++ src/main/java/net/spit365/lulasmod/mod/ModStructures.java
@@ -1,61 +1,69 @@
 package net.spit365.lulasmod.mod;
 
-import net.fabricmc.fabric.api.event.registry.DynamicRegistrySetupCallback;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerChunkEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.block.Block;
+import net.minecraft.block.entity.StructureBlockBlockEntity;
 import net.minecraft.registry.*;
-import net.minecraft.registry.entry.RegistryEntry;
-import net.minecraft.registry.entry.RegistryEntryList;
-import net.minecraft.registry.tag.BiomeTags;
+import net.minecraft.structure.StructurePlacementData;
 import net.minecraft.structure.StructureSet;
+import net.minecraft.structure.StructureTemplate;
+import net.minecraft.structure.StructureTemplateManager;
+import net.minecraft.structure.processor.BlockRotStructureProcessor;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.Vec3i;
-import net.minecraft.world.biome.Biome;
-import net.minecraft.world.gen.chunk.placement.ConcentricRingsStructurePlacement;
-import net.minecraft.world.gen.chunk.placement.StructurePlacement;
-import net.minecraft.world.gen.structure.Structure;
+import net.minecraft.util.InvalidIdentifierException;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.MathHelper;
 import net.spit365.lulasmod.Lulasmod;
 import net.spit365.lulasmod.custom.item.spell.ConjuringItem;
 
-import java.util.List;
+import javax.swing.*;
+import java.util.Arrays;
 import java.util.Optional;
 
 public final class ModStructures {
     public static int RADIUS_BLOCKS = 1000;
     public static final Identifier GAZEBO_OF_SINS_ID = Identifier.of(Lulasmod.MOD_ID, "gazebo_of_sins");
     public static final RegistryKey<StructureSet> RING_SET_KEY = RegistryKey.of(RegistryKeys.STRUCTURE_SET, Identifier.of(Lulasmod.MOD_ID, "structure_ring"));
+    private static final int count = (int) ModSpells.SpellTabItems.stream().filter(identifier -> !(Registries.ITEM.get(identifier) instanceof ConjuringItem)).count();
+    private static final BlockPos[] GAZEBO_POS = new BlockPos[count];
 
-    private static int getCount() {
-        return (int) ModSpells.SpellTabItems.stream().filter(identifier -> !(Registries.ITEM.get(identifier) instanceof ConjuringItem)).count();
+
+    static {
+        Arrays.setAll(GAZEBO_POS, value -> {
+            double angle = (Math.PI * 2)  * ((double) value / count);
+            return new BlockPos(
+                (int) Math.round(Math.cos(angle) * RADIUS_BLOCKS),
+                0,
+                (int) Math.round(Math.sin(angle) * RADIUS_BLOCKS));
+        });
     }
 
     public static void init(){
-        DynamicRegistrySetupCallback.EVENT.register(view -> {
-            Optional<Registry<Biome>> biomeRegistryOpt = view.getOptional(RegistryKeys.BIOME);
-            Optional<Registry<Structure>> structureRegistryOpt = view.getOptional(RegistryKeys.STRUCTURE);
-            Optional<Registry<StructureSet>> setRegistryOpt = view.getOptional(RegistryKeys.STRUCTURE_SET);
+        ServerChunkEvents.CHUNK_LOAD.register((serverWorld, worldChunk) -> {
+            for (BlockPos pos : GAZEBO_POS){
+                if (worldChunk.getPos().equals(new ChunkPos(pos))){
+                    if (serverWorld.getServer().isOnThread()) {
+                        StructureTemplateManager structureTemplateManager = serverWorld.getStructureTemplateManager();
+                        Optional<StructureTemplate> optional = Optional.empty();
+                        try {
+                            optional = structureTemplateManager.getTemplate(GAZEBO_OF_SINS_ID);
+                        } catch (InvalidIdentifierException ignored) {
+                        }
 
-            if (biomeRegistryOpt.isEmpty() || structureRegistryOpt.isEmpty() || setRegistryOpt.isEmpty()) {
-                Lulasmod.LOGGER.error("Missing one of the dynamic registries!");
-                return;
+                        optional.ifPresent(structureTemplate -> structureTemplate.place(
+                                serverWorld,
+                                pos,
+                                pos,
+                                new StructurePlacementData(),
+                                serverWorld.getRandom(),
+                                Block.NOTIFY_LISTENERS | Block.FORCE_STATE_AND_SKIP_CALLBACKS_AND_DROPS
+                        ));
+                        break;
+                    }
+                }
             }
-
-            RegistryEntryList.Named<Biome> preferredOverworld = biomeRegistryOpt.get().getOrThrow(BiomeTags.IS_OVERWORLD);
-            RegistryEntry<Structure> myStructureEntry =
-                    structureRegistryOpt.get().getEntry(GAZEBO_OF_SINS_ID)
-                            .orElseThrow(() -> new IllegalStateException("Structure " + GAZEBO_OF_SINS_ID + " not found"));
-
-            ConcentricRingsStructurePlacement placement = new ConcentricRingsStructurePlacement(
-                    new Vec3i(0, 0, 0),
-                    StructurePlacement.FrequencyReductionMethod.DEFAULT,
-                    1.0f,
-                    0,
-                    Optional.empty(),
-                    Math.round(RADIUS_BLOCKS / 16f),
-                    1,
-                    getCount(),
-                    preferredOverworld
-            );
-
-            Registry.register(setRegistryOpt.get(), RING_SET_KEY.getValue(), new StructureSet(List.of(StructureSet.createEntry(myStructureEntry)), placement));
         });
     }
 }
