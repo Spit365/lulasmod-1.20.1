a4e4fc0374b26fceedc0330c95b43d48ff992e3d
Spit365 <lukas.grossauer47@gmail.com>
2025-10-22 16:14:29 +0200

    made the pin sword render

--- src/main/java/net/spit365/lulasmod/custom/entity/renderer/PinSwordEntityRenderer.java
+++ src/main/java/net/spit365/lulasmod/custom/entity/renderer/PinSwordEntityRenderer.java
@@ -1,23 +1,65 @@
 package net.spit365.lulasmod.custom.entity.renderer;
 
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.OverlayTexture;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
-import net.minecraft.client.render.entity.ProjectileEntityRenderer;
 import net.minecraft.client.render.entity.state.ProjectileEntityRenderState;
-import net.minecraft.util.Identifier;
+import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.ItemDisplayContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.RotationAxis;
+import net.minecraft.world.World;
 import net.spit365.lulasmod.custom.entity.PinSwordEntity;
 
-public class PinSwordEntityRenderer extends ProjectileEntityRenderer<PinSwordEntity, ProjectileEntityRenderState> {
-    public PinSwordEntityRenderer(EntityRendererFactory.Context context) {
-        super(context);
+public class PinSwordEntityRenderer extends EntityRenderer<PinSwordEntity, PinSwordEntityRenderer.PinSwordRenderState> {
+    private final ItemRenderer itemRenderer;
+
+    public PinSwordEntityRenderer(EntityRendererFactory.Context ctx) {
+        super(ctx);
+        this.itemRenderer = MinecraftClient.getInstance().getItemRenderer();
     }
 
     @Override
-    protected Identifier getTexture(ProjectileEntityRenderState state) {
-        return null;
+    public PinSwordRenderState createRenderState() {
+        return new PinSwordRenderState();
     }
 
     @Override
-    public ProjectileEntityRenderState createRenderState() {
-        return new ProjectileEntityRenderState();
+    public void render(PinSwordRenderState state, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light) {
+
+        matrices.push();
+        matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(-state.pitch + 75));
+        matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(-state.yaw));
+
+        itemRenderer.renderItem(
+            state.pinSword,
+            ItemDisplayContext.FIRST_PERSON_LEFT_HAND,
+            light,
+            OverlayTexture.DEFAULT_UV,
+            matrices,
+            vertexConsumers,
+            state.world,
+            0
+        );
+
+        matrices.pop();
+    }
+
+    @Override
+    public void updateRenderState(PinSwordEntity entity, PinSwordRenderState state, float tickProgress) {
+        super.updateRenderState(entity, state, tickProgress);
+        state.pinSword = entity.getSword();
+        state.world = entity.getWorld();
+        state.yaw = entity.getYaw();
+        state.pitch = entity.getPitch();
+    }
+
+    public static class PinSwordRenderState extends ProjectileEntityRenderState {
+        public ItemStack pinSword;
+        public World world;
+
     }
 }
