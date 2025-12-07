f3d8108fb771ba0a7d4283a1feeaf6bcd5ef42ab
Spit365 <lukas.grossauer47@gmail.com>
2025-12-07 20:21:55 +0100

    Modified Bleed

--- src/main/java/net/spit365/lulasmod/custom/Bleed.java
+++ src/main/java/net/spit365/lulasmod/custom/Bleed.java
@@ -1,7 +1,7 @@
 package net.spit365.lulasmod.custom;
 
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.spit365.lulasmod.mod.ModDamageSources;
 import net.spit365.lulasmod.mod.ModData;
@@ -9,16 +9,20 @@ import net.spit365.lulasmod.mod.ModData;
 import java.util.stream.StreamSupport;
 
 public class Bleed {
-    public static void tick(ServerWorld serverWorld){
-        StreamSupport.stream(serverWorld.iterateEntities().spliterator(), true).filter(entity -> entity instanceof LivingEntity && entity.getAttached(ModData.BLEED_VALUE) != null).map(LivingEntity.class::cast).forEach(entity -> {
+    public static void tick(ServerWorld world){
+        StreamSupport.stream(world.iterateEntities().spliterator(), true).filter(entity -> entity instanceof LivingEntity && entity.getAttached(ModData.BLEED_VALUE) != null).map(LivingEntity.class::cast).forEach(entity -> {
             Integer duration = entity.getAttached(ModData.BLEED_VALUE);
             if (duration == null) return;
-            int min = Math.min((int) (Math.min(entity.getHealth(), entity.getMaxHealth()) * 60) - 1, 1200);
-            if (duration > min) {
-                entity.setAttached(ModData.BLEED_VALUE, duration - min);
-                entity.damage(serverWorld, ModDamageSources.bloodsucking(entity), entity.getMaxHealth() * 0.15f + 10f);
-                serverWorld.spawnParticles(ParticleTypes.EFFECT, entity.getX(), entity.getY(), entity.getZ(), 5, 1, 1, 1, 1);
+            int threshold = Math.min((int) (Math.min(entity.getHealth(), entity.getMaxHealth()) * 60), 1200);
+            if (duration > threshold) {
+                duration -= threshold;
+                entity.damage(world, ModDamageSources.bloodsucking(entity), entity.getMaxHealth() * 0.15f + 10f);
+                StreamSupport.stream(world.iterateEntities().spliterator(), true).filter(target -> target.squaredDistanceTo(entity) < 1000000 && target instanceof ServerPlayerEntity).forEach(player -> {
+                    //ServerPlayNetworking.send((ServerPlayerEntity) player, (CustomPayload) (Object) new SummonBleedS2CPacket(entity.getX(), entity.getY(), entity.getZ()));
+                });
             }
+            duration--;
+            entity.setAttached(ModData.BLEED_VALUE, duration);
         });
     }
 }
