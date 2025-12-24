f27f66a54e557daca2c1bff92d3208319e226ad4
Spit365 <lukas.grossauer47@gmail.com>
2025-12-24 12:07:14 +0100

    fixed a bug when bleed <= 0

--- src/main/java/net/spit365/lulasmod/custom/Bleed.java
+++ src/main/java/net/spit365/lulasmod/custom/Bleed.java
@@ -3,12 +3,12 @@ package net.spit365.lulasmod.custom;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.client.gl.RenderPipelines;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.spit365.lulasmod.Lulasmod;
 import net.spit365.lulasmod.mod.ModDamageSources;
@@ -26,7 +26,7 @@ public class Bleed {
 
     public static void tick(ServerWorld world){
         StreamSupport.stream(world.iterateEntities().spliterator(), false).<LivingEntity>mapMulti((target, result) -> {
-            if (target instanceof LivingEntity livingEntity) result.accept(livingEntity);
+            if (target instanceof LivingEntity livingEntity && target.isAlive()) result.accept(livingEntity);
         }).forEach(target -> {
             Integer duration = target.getAttached(ModData.BLEED_VALUE);
             if (duration == null) return;
@@ -34,9 +34,10 @@ public class Bleed {
             if (threshold <= 0) return;
             if (duration >= threshold) {
                 target.damage(world, ModDamageSources.bloodsucking(world), (target.getMaxHealth() * 0.15f + 10f) * duration / threshold);
-                world.getPlayers().stream()
-				    .filter(player -> player.squaredDistanceTo(target) < 1000000 /*1000²*/)
-					.forEach(player -> ServerPlayNetworking.send(player, new SummonBleedS2CPacket(target.getX(), target.getY(), target.getZ())));
+                for (ServerPlayerEntity player : world.getPlayers()) {
+                    if (player.squaredDistanceTo(target) > 1000000) continue;
+                    ServerPlayNetworking.send(player, new SummonBleedS2CPacket(target.getX(), target.getY(), target.getZ()));
+                }
             }
             duration = duration % threshold - 1;
             if (duration > 0) target.setAttached(ModData.BLEED_VALUE, duration);
@@ -46,12 +47,11 @@ public class Bleed {
 
     public static void tick(ServerPlayerEntity player){
         Integer duration = player.getAttached(ModData.BLEED_VALUE);
-        if (duration != null)
-            ServerPlayNetworking.send(player, new BleedProgressS2CPacket(duration * 100 / Math.max(getThreshold(player), 1)));
+        ServerPlayNetworking.send(player, new BleedProgressS2CPacket(duration != null ? duration * 100 / getThreshold(player) : 0));
     }
 
     private static int getThreshold(LivingEntity entity) {
-        return Math.min((int) entity.getHealth(), 20) * 60;
+        return MathHelper.clamp((int) entity.getHealth() * 60, 1, 1200) ;
     }
 
     public static void apply(LivingEntity entity, int duration){
@@ -65,7 +65,7 @@ public class Bleed {
         }
 	}
 
-    public static void render(DrawContext drawContext, RenderTickCounter renderTickCounter) {
+    public static void render(DrawContext drawContext) {
         int textWidth = 182;
         int textHeight = 5;
         int x = (drawContext.getScaledWindowWidth() - textWidth) / 2;
