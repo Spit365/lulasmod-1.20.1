ace4ba5e0eeabe5b7d698f83f06bccea2628a03f
Spit365 <lukas.grossauer47@gmail.com>
2026-01-04 21:48:33 +0100

    cleanup in the SpellHotbar class

--- src/main/java/net/spit365/lulasmod/util/SpellHotbar.java
+++ src/main/java/net/spit365/lulasmod/util/SpellHotbar.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
 import net.spit365.lulasmod.packet.SpellHotbarListS2CPacket;
 
@@ -15,13 +16,12 @@ public interface SpellHotbar {
     void onCycle(PlayerEntity player, int value);
 
     static void tick(ServerPlayerEntity player) {
-        if (player.getMainHandStack().getItem() instanceof SpellHotbar item)
-            sendSpellListPacket(player, item.getHotbarList(player));
-        else if (player.getOffHandStack().getItem() instanceof SpellHotbar item)
-            sendSpellListPacket(player, item.getHotbarList(player));
+        for (Hand hand : Hand.values()) {
+            if (!(player.getStackInHand(hand).getItem() instanceof SpellHotbar item)) continue;
+            List<Identifier> hotbarList = item.getHotbarList(player);
+            if (hotbarList != null) ServerPlayNetworking.send(player, new SpellHotbarListS2CPacket(hotbarList.stream().map(id -> new ItemStack(Registries.ITEM.get(id))).toList()));
+            break;
+        }
     }
 
-    static void sendSpellListPacket(ServerPlayerEntity player, List<Identifier> list) {
-        if (list != null) ServerPlayNetworking.send(player, new SpellHotbarListS2CPacket(list.stream().map(id -> new ItemStack(Registries.ITEM.get(id))).toList()));
-    }
 }
