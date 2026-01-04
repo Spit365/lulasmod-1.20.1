7fbb9dacb011a9e2cf8e71607bd2deb36be3b657
Spit365 <lukas.grossauer47@gmail.com>
2026-01-04 21:47:26 +0100

    null check built into the is demon method

--- src/main/java/net/spit365/lulasmod/custom/Demon.java
+++ src/main/java/net/spit365/lulasmod/custom/Demon.java
@@ -2,13 +2,14 @@ package net.spit365.lulasmod.custom;
 
 import net.minecraft.entity.Entity;
 import net.spit365.lulasmod.mod.ModData;
+import org.jetbrains.annotations.Nullable;
 
 public class Demon {
 	public static void setDemon(Entity entity, boolean value) {
 		entity.setAttached(ModData.DEMON, value);
 	}
 
-	public static boolean isDemon(Entity entity) {
-		return Boolean.TRUE.equals(entity.getAttached(ModData.DEMON));
+	public static boolean isDemon(@Nullable Entity entity) {
+		return entity != null && Boolean.TRUE.equals(entity.getAttached(ModData.DEMON));
 	}
 }
