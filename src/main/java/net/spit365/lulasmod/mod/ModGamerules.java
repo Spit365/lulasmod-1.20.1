13fa1d08c13bf9b64403c2fc8cd0d1056a0df99e
Spit365 <lukas.grossauer47@gmail.com>
2026-01-31 13:50:24 +0100

    extracted Identifier.of(Lulasmod.MOD_ID, id) to id(name)

--- src/main/java/net/spit365/lulasmod/mod/ModGamerules.java
+++ src/main/java/net/spit365/lulasmod/mod/ModGamerules.java
@@ -3,8 +3,10 @@ package net.spit365.lulasmod.mod;
 import net.minecraft.world.GameRules;
 import net.spit365.lulasmod.util.RegisterHelper;
 
+import static net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory.*;
+
 public final class ModGamerules {
-	public static final GameRules.Key<GameRules.BooleanRule> NEW_DEATH_SYSTEM = RegisterHelper.gameRule("newDeathSystem", GameRules.Category.PLAYER, false);
+	public static final GameRules.Key<GameRules.BooleanRule> NEW_DEATH_SYSTEM = RegisterHelper.gameRule("newDeathSystem", GameRules.Category.PLAYER, createBooleanRule(false));
 
 	public static void init() {}
 }
--- src/main/java/net/spit365/lulasmod/util/RegisterHelper.java
+++ src/main/java/net/spit365/lulasmod/util/RegisterHelper.java
@@ -3,7 +3,6 @@ package net.spit365.lulasmod.util;
 import com.mojang.serialization.Codec;
 import net.fabricmc.fabric.api.attachment.v1.AttachmentRegistry;
 import net.fabricmc.fabric.api.attachment.v1.AttachmentType;
-import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
@@ -35,6 +34,7 @@ import net.spit365.lulasmod.mod.ModBlocks;
 import net.spit365.lulasmod.mod.ModData;
 import net.spit365.lulasmod.mod.ModItems;
 import net.spit365.lulasmod.mod.ModSpells;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -44,13 +44,13 @@ import java.util.function.UnaryOperator;
 
 public final class RegisterHelper {
 	public static <T> AttachmentType<T> attachmentType(String name, boolean deathPersistent){
-		AttachmentType<T> attachmentType = AttachmentRegistry.create(Identifier.of(Lulasmod.MOD_ID, name));
+		AttachmentType<T> attachmentType = AttachmentRegistry.create(id(name));
 		if (deathPersistent) ModData.deathPersistent.add((AttachmentType<Object>) attachmentType);
 		return attachmentType;
 	}
 
 	public static <T extends Block> ModBlocks.BlockAndItem<T> block(String name, Function<AbstractBlock.Settings, T> factory, AbstractBlock.Settings settings) {
-		Identifier id = Identifier.of(Lulasmod.MOD_ID, name);
+		Identifier id = id(name);
 		RegistryKey<Block> blockRegistryKey = RegistryKey.of(RegistryKeys.BLOCK, id);
 		RegistryKey<Item> itemRegistryKey = RegistryKey.of(RegistryKeys.ITEM, id);
 		T block = factory.apply(settings.registryKey(blockRegistryKey));
@@ -63,20 +63,20 @@ public final class RegisterHelper {
 	}
 
 	public static <T> ComponentType<T> componentType(String name, UnaryOperator<ComponentType.Builder<T>> builderOperator) {
-		return Registry.register(Registries.DATA_COMPONENT_TYPE, Identifier.of(Lulasmod.MOD_ID, name),
+		return Registry.register(Registries.DATA_COMPONENT_TYPE, id(name),
 				builderOperator.apply(ComponentType.builder()).build());
 	}
 
 	public static RegistryKey<DamageType> damageType(String name) {
-		return RegistryKey.of(RegistryKeys.DAMAGE_TYPE, Identifier.of(Lulasmod.MOD_ID, name));
+		return RegistryKey.of(RegistryKeys.DAMAGE_TYPE, id(name));
 	}
 
 	public static RegistryKey<World> dimension(String name) {
-		return RegistryKey.of(RegistryKeys.WORLD, Identifier.of(Lulasmod.MOD_ID, name));
+		return RegistryKey.of(RegistryKeys.WORLD, id(name));
 	}
 
 	public static <T extends Entity> EntityType<T> entity(String id, EntityType.EntityFactory<T> entityFactory, float width, float height, int maxTrackingRange, int trackingTickInterval) {
-		RegistryKey<EntityType<?>> key = RegistryKey.of(RegistryKeys.ENTITY_TYPE, Identifier.of(Lulasmod.MOD_ID, id));
+		RegistryKey<EntityType<?>> key = RegistryKey.of(RegistryKeys.ENTITY_TYPE, id(id));
         return Registry.register(Registries.ENTITY_TYPE, key,
 			 EntityType.Builder.create(entityFactory, SpawnGroup.MISC)
 				 .dimensions(width, height)
@@ -85,8 +85,8 @@ public final class RegisterHelper {
 				 .build(key));
 	}
 
-	public static GameRules.Key<GameRules.BooleanRule> gameRule(String name, GameRules.Category category, boolean defaultValue) {
-		return GameRuleRegistry.register(name, category, GameRuleFactory.createBooleanRule(defaultValue));
+	public static GameRules.Key<GameRules.BooleanRule> gameRule(String name, GameRules.Category category, GameRules.Type<GameRules.BooleanRule> type) {
+		return GameRuleRegistry.register(Lulasmod.MOD_ID + "." + name, category, type);
 	}
 
 	public static <T extends Item> T item(String name, Function<Item.Settings, T> factory, Item.Settings settings) {
@@ -102,12 +102,12 @@ public final class RegisterHelper {
 		return item;
 	}
 	private static <T extends Item> T itemInternal(String name, Function<Item.Settings, T> factory, Item.Settings settings) {
-		RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Lulasmod.MOD_ID, name));
+		RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, id(name));
 		return Registry.register(Registries.ITEM, key, factory.apply(settings.registryKey(key)));
 	}
 
 	public static ItemGroup itemGroup(String name, Item item, List<ItemStack> list) {
-		return Registry.register(Registries.ITEM_GROUP, Identifier.of(Lulasmod.MOD_ID, name), FabricItemGroup.builder()
+		return Registry.register(Registries.ITEM_GROUP, id(name), FabricItemGroup.builder()
 			.displayName(Text.translatable("item_group." + Lulasmod.MOD_ID + "." + name))
 			.icon(() -> new ItemStack(item))
 			.entries((displayContext, entries) -> entries.addAll(list))
@@ -116,12 +116,12 @@ public final class RegisterHelper {
 
 	public static SimpleParticleType particle(String name, Boolean alwaysShow){
 		 SimpleParticleType particle = FabricParticleTypes.simple(alwaysShow);
-		 Registry.register(Registries.PARTICLE_TYPE, Identifier.of(Lulasmod.MOD_ID, name), particle);
+		 Registry.register(Registries.PARTICLE_TYPE, id(name), particle);
 		 return particle;
 	}
 
 	public static <T> AttachmentType<T> persistentAttachmentType(String name, Codec<T> codec, boolean deathPersistent){
-		AttachmentType<T> attachmentType = AttachmentRegistry.createPersistent(Identifier.of(Lulasmod.MOD_ID, name), codec);
+		AttachmentType<T> attachmentType = AttachmentRegistry.createPersistent(id(name), codec);
 		if (deathPersistent) ModData.deathPersistent.add((AttachmentType<Object>) attachmentType);
 		return attachmentType;
 	}
@@ -132,8 +132,12 @@ public final class RegisterHelper {
 		return spell;
 	}
 
-	public static RegistryEntry<StatusEffect> statusEffect(String id, StatusEffect effect) {
-		Registry.register(Registries.STATUS_EFFECT, Identifier.of(Lulasmod.MOD_ID, id), effect);
+	public static RegistryEntry<StatusEffect> statusEffect(String name, StatusEffect effect) {
+		Registry.register(Registries.STATUS_EFFECT, id(name), effect);
 		return Registries.STATUS_EFFECT.getEntry(effect);
+	}
+
+	private static @NotNull Identifier id(String name) {
+		return Identifier.of(Lulasmod.MOD_ID, name);
 	}
 }
