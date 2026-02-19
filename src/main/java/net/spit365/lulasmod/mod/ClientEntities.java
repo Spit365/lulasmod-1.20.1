298139fe4ba972d7097ae0e33ddaa09693987888
Spit365 <lukas.grossauer47@gmail.com>
2026-02-19 15:47:15 +0100

    replaced EmptyRenderer with mojang's identical EmptyEntityRenderer

--- src/main/java/net/spit365/lulasmod/mod/ClientEntities.java
+++ src/main/java/net/spit365/lulasmod/mod/ClientEntities.java
@@ -1,8 +1,8 @@
 package net.spit365.lulasmod.mod;
 
 import net.minecraft.client.render.entity.FlyingItemEntityRenderer;
+import net.minecraft.client.render.entity.EmptyEntityRenderer;
 import net.spit365.lulasmod.renderer.AmethystShardEntityRenderer;
-import net.spit365.lulasmod.renderer.EmptyRenderer;
 import net.spit365.lulasmod.renderer.NeedleSwordEntityRenderer;
 import net.spit365.lulasmod.util.ClientRegisterHelper;
 
@@ -12,9 +12,9 @@ public final class ClientEntities {
 
 	public static void init() {
 		ClientRegisterHelper.entity(SMOKE_BOMB, FlyingItemEntityRenderer::new);
-		ClientRegisterHelper.entity(SMOKE_PROJECTILE, EmptyRenderer::new);
+		ClientRegisterHelper.entity(SMOKE_PROJECTILE, EmptyEntityRenderer::new);
 		ClientRegisterHelper.entity(MALIGNITY, FlyingItemEntityRenderer::new);
-		ClientRegisterHelper.entity(PARTICLE_PROJECTILE, EmptyRenderer::new);
+		ClientRegisterHelper.entity(PARTICLE_PROJECTILE, EmptyEntityRenderer::new);
 		ClientRegisterHelper.entity(AMETHYST_SHARD, AmethystShardEntityRenderer::new);
 		ClientRegisterHelper.entity(NEEDLE_SWORD, NeedleSwordEntityRenderer::new);
 	}
