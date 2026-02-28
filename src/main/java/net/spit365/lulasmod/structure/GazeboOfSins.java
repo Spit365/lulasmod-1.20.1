50d8143e842bccfd6d6ec411b16d512dd1249047
Spit365 <lukas.grossauer47@gmail.com>
2026-02-28 23:31:23 +0100

    finally fixed gazebo generation

--- src/main/java/net/spit365/lulasmod/structure/GazeboOfSins.java
+++ src/main/java/net/spit365/lulasmod/structure/GazeboOfSins.java
@@ -3,36 +3,42 @@ package net.spit365.lulasmod.structure;
 import net.minecraft.block.Block;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.structure.StructurePlacementData;
+import net.minecraft.structure.StructureTemplate;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.*;
 import net.spit365.lulasmod.Lulasmod;
 import net.spit365.lulasmod.state.GazeboGenerationPersistentState;
 
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Optional;
 
 public final class GazeboOfSins {
     public static final Identifier GAZEBO_OF_SINS_ID = Identifier.of(Lulasmod.MOD_ID, "gazebo_of_sins");
 
-	public static void tick(ServerWorld serverWorld){
-		GazeboGenerationPersistentState gazeboGenerationPersistentState = GazeboGenerationPersistentState.get(serverWorld);
-		if (gazeboGenerationPersistentState == null) return;
-        HashSet<Map.Entry<BlockPos, Boolean>> gazeboPos = new HashSet<>(gazeboGenerationPersistentState.getPending(serverWorld).entrySet());
-        for (Map.Entry<BlockPos, Boolean> entry : gazeboPos){
-            if (!entry.getValue()) return;
+    public static void tick(ServerWorld world) {
+        GazeboGenerationPersistentState gazeboGenerationPersistentState = GazeboGenerationPersistentState.get(world);
+        if (gazeboGenerationPersistentState == null) return;
+        HashSet<Map.Entry<BlockPos, Boolean>> gazeboPos = new HashSet<>(gazeboGenerationPersistentState.getPending(world).entrySet());
+
+        for (Map.Entry<BlockPos, Boolean> entry : gazeboPos) {
             BlockPos pos = entry.getKey();
-            ChunkPos chunkPos = new ChunkPos(pos);
-            if (!serverWorld.getChunkManager().isChunkLoaded(chunkPos.x, chunkPos.z)) continue;
-            serverWorld.getStructureTemplateManager().getTemplate(GAZEBO_OF_SINS_ID).ifPresent(structureTemplate -> structureTemplate.place(
-                serverWorld,
+            if (entry.getValue()) continue;
+            Optional<StructureTemplate> structureTemplate = world.getStructureTemplateManager().getTemplate(GAZEBO_OF_SINS_ID);
+            if (structureTemplate.isEmpty()) continue;
+
+            StructureTemplate gazebo = structureTemplate.get();
+            boolean loaded = ChunkPos.stream(new ChunkPos(pos), new ChunkPos(pos.add(gazebo.getSize())))
+                .allMatch(chunkPos -> world.isPosLoaded(chunkPos.getStartPos()));
+            boolean placed = gazebo.place(
+                world,
                 pos,
                 pos,
                 new StructurePlacementData(),
-                serverWorld.getRandom(),
-                Block.FORCE_STATE_AND_SKIP_CALLBACKS_AND_DROPS
-            ));
-            gazeboGenerationPersistentState.markAsLoaded(pos);
+                world.getRandom(),
+                Block.NOTIFY_LISTENERS
+            );
+            if (loaded && placed) gazeboGenerationPersistentState.markAsLoaded(pos);
         }
-	}
+    }
 }
