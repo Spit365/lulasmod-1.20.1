b925db53ce5ba1494bf4e37952beef66656da3af
Spit365 <lukas.grossauer47@gmail.com>
2026-03-30 12:43:15 +0200

    made ClientScreens final

--- src/main/java/net/spit365/lulasmod/mod/ClientScreens.java
+++ src/main/java/net/spit365/lulasmod/mod/ClientScreens.java
@@ -3,7 +3,7 @@ package net.spit365.lulasmod.mod;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
 import net.spit365.lulasmod.screen.TitrationStandScreen;
 
-public class ClientScreens {
+public final class ClientScreens {
     public static void init(){
         HandledScreens.register(ModScreenHandlers.TITRATION_STAND_SCREEN_HANDLER, TitrationStandScreen::new);
     }
