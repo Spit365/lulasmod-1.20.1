b7db9729ccb08f9234e22d9988fe4da507203b17
Spit365 <lukas.grossauer47@gmail.com>
2026-03-30 19:31:06 +0200

    abstraction layer for keybinds that send packets

--- src/main/java/net/spit365/lulasmod/mod/ModKeybinds.java
+++ src/main/java/net/spit365/lulasmod/mod/ModKeybinds.java
@@ -2,8 +2,6 @@ package net.spit365.lulasmod.mod;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.option.KeyBinding;
 import net.spit365.lulasmod.packet.BreakWithMindC2SPacket;
 import net.spit365.lulasmod.packet.CycleSpellHotbarC2SPacket;
@@ -14,17 +12,10 @@ import org.lwjgl.glfw.GLFW;
 
 @Environment(EnvType.CLIENT)
 public final class ModKeybinds {
-	public static final KeyBinding CYCLE_SPELL_KEY = ClientRegisterHelper.keyBinding("cycle_spell", GLFW.GLFW_KEY_R);
-	public static final KeyBinding DEMON_CONTRACT_KEY = ClientRegisterHelper.keyBinding("demon_contract", GLFW.GLFW_KEY_BACKSLASH);
-    public static final KeyBinding DASH_KEY = ClientRegisterHelper.keyBinding("dash", GLFW.GLFW_KEY_C);
-    public static final KeyBinding BREAK_WITH_MIND = ClientRegisterHelper.keyBinding("break_with_mind", GLFW.GLFW_KEY_G);
+	public static final KeyBinding CYCLE_SPELL_KEY = ClientRegisterHelper.packetKeyBinding("cycle_spell", GLFW.GLFW_KEY_R, true, CycleSpellHotbarC2SPacket::new);
+	public static final KeyBinding DEMON_CONTRACT_KEY = ClientRegisterHelper.packetKeyBinding("demon_contract", GLFW.GLFW_KEY_BACKSLASH, false, DemonContractC2SPacket::new);
+    public static final KeyBinding DASH_KEY = ClientRegisterHelper.packetKeyBinding("dash", GLFW.GLFW_KEY_C, true, DashC2SPacket::new);
+    public static final KeyBinding BREAK_WITH_MIND = ClientRegisterHelper.packetKeyBinding("break_with_mind", GLFW.GLFW_KEY_G, true, BreakWithMindC2SPacket::new);
 
-	public static void init(){
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (ModKeybinds.CYCLE_SPELL_KEY.wasPressed()) ClientPlayNetworking.send(new CycleSpellHotbarC2SPacket());
-            if (ModKeybinds.DEMON_CONTRACT_KEY.wasPressed()) ClientPlayNetworking.send(new DemonContractC2SPacket());
-            if (ModKeybinds.DASH_KEY.isPressed()) ClientPlayNetworking.send(new DashC2SPacket());
-            if (ModKeybinds.BREAK_WITH_MIND.wasPressed()) ClientPlayNetworking.send(new BreakWithMindC2SPacket());
-        });
-    }
+	public static void init() {}
 }
--- src/main/java/net/spit365/lulasmod/util/ClientRegisterHelper.java
+++ src/main/java/net/spit365/lulasmod/util/ClientRegisterHelper.java
@@ -1,6 +1,8 @@
 package net.spit365.lulasmod.util;
 
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElement;
@@ -10,16 +12,19 @@ import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.network.packet.CustomPayload;
 import net.minecraft.particle.SimpleParticleType;
 import net.minecraft.util.Identifier;
 import net.spit365.lulasmod.Lulasmod;
+
+import java.util.function.Supplier;
 
 public final class ClientRegisterHelper {
 	public static <T extends Entity> void entity(EntityType<T> entityType, EntityRendererFactory<T> entityRendererFactory) {
 		EntityRendererRegistry.register(entityType, entityRendererFactory);
 	}
 
-	public static KeyBinding keyBinding(String name, int key){
+	public static KeyBinding keyBinding(String name, int key) {
 		return KeyBindingHelper.registerKeyBinding(new KeyBinding(
 			"key." + Lulasmod.MOD_ID + "." + name,
 			InputUtil.Type.KEYSYM,
@@ -28,7 +33,20 @@ public final class ClientRegisterHelper {
 		));
 	}
 
-	public static void particle(SimpleParticleType particle, ParticleFactoryRegistry.PendingParticleFactory<SimpleParticleType> render){
+	public static KeyBinding packetKeyBinding(String name, int key, boolean repeating, Supplier<CustomPayload> payloadSupplier) {
+		KeyBinding keyBinding = keyBinding(name, key);
+        ClientTickEvents.END_CLIENT_TICK.register(repeating ?
+            client -> {
+				if (keyBinding.wasPressed()) ClientPlayNetworking.send(payloadSupplier.get());
+			} :
+            client -> {
+				if (keyBinding.isPressed()) ClientPlayNetworking.send(payloadSupplier.get());
+			}
+		);
+		return keyBinding;
+	}
+
+	public static void particle(SimpleParticleType particle, ParticleFactoryRegistry.PendingParticleFactory<SimpleParticleType> render) {
 		 ParticleFactoryRegistry.getInstance().register(particle, render);
 	}
 
